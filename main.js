cfb341f096161ce247f2c9e99b0b5c44c152dedc
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-31 23:04:28 +0300

    Done with Lab

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const Hearts = document.querySelectorAll(".like-glyph");
+let modal = document.querySelector("#modal");
 
+function like(e){
+  const heart = e.target;
+  mimicServerCall()
+  .then(function(serverMessage){
+    if(heart.innerText === EMPTY_HEART){
+      alert("You successfully liked the post!");
+      heart.innerText = FULL_HEART
+      heart.classList.add('activated-heart');
+    } else{
+      alert("You unliked the post!")
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove('activated-heart')
+    }
+    })
+    .catch( (error) => {
+      modal.classList.remove('hidden');
+        setTimeout( function(){
+          modal.classList.add('hidden');
+        },3);
+    });
+}
 
-
+for (const click of Hearts) {
+  click.addEventListener("click", like);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
